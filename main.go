9dd04d7c527574e37c35d8157b5890b70586a946
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-02-28 14:17:39 -0800

    An interrupt fired

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"device/sam"
 	"fmt"
 	"machine"
+	"runtime/interrupt"
+	"runtime/volatile"
 	"time"
 	neopixel_spi "uc-go/pkg/neopixel-spi"
 )
@@ -13,13 +15,18 @@ var (
 	ledPin = machine.PA23
 )
 
-func main() {
-	spi := machine.SPI{Bus: sam.SERCOM5_SPIM, SERCOM: 5}
+var interruptCount uint8 = 0
+var spi *machine.SPI
+var intr interrupt.Interrupt
 
-	//machine.SPI0
-	//.0 SDO MOSI
-	//.1 CLK
-	//.3 MISO
+func spiInterruptHandler(i interrupt.Interrupt) {
+	intr.Disable()
+	volatile.StoreUint8(&interruptCount, 1)
+	spi.Bus.INTFLAG.Set(sam.SERCOM_SPIM_INTFLAG_DRE)
+}
+
+func main() {
+	spi = &machine.SPI{Bus: sam.SERCOM5_SPIM, SERCOM: 5}
 
 	err := spi.Configure(machine.SPIConfig{
 		Frequency: 2_400_000,
@@ -29,6 +36,11 @@ func main() {
 		LSBFirst:  true,
 		Mode:      0,
 	})
+
+	spi.Bus.INTENSET.Set(sam.SERCOM_SPIM_INTENSET_DRE)
+	intr = interrupt.New(sam.IRQ_SERCOM5_0, spiInterruptHandler)
+	intr.Enable()
+
 	if err != nil {
 		forever(err)
 	}
@@ -62,8 +74,17 @@ func main() {
 
 	buf = append(buf, space...)
 
-	i := 0
 	for {
+		loop(buf)
+		fmt.Println("hello", volatile.LoadUint8(&interruptCount))
+	}
+
+}
+
+func loop(buf []byte) {
+	i := 0
+	count := 100
+	for count > 0 {
 		for !spi.Bus.INTFLAG.HasBits(sam.SERCOM_SPIM_INTFLAG_DRE) {
 		}
 		val := buf[i]
@@ -71,11 +92,8 @@ func main() {
 		i++
 		if i >= len(buf) {
 			i = 0
+			count--
 		}
-		//err := spi.Tx(buf, nil)
-		//if err != nil {
-		//	forever(err)
-		//}
 	}
 }
 
